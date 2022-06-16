ad1860d8a66fd4099edae654d603c0dc61933f1c
austinhooker <austin.hooker@ruralsourcing.com>
2022-06-16 17:22:57 -0400

    AddedTypeAheadNotWorkingForMovies

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult CreateNewRental(NewRentalDto rentalDto)
+        {
+            var customer = _context.Customers.Single(c => c.Id == rentalDto.CustomerId);
+            
+            var moviesList = _context.Movies.Where(m => rentalDto.MovieIds.Contains(m.Id)).ToList();
+            foreach(var movie in moviesList)
+            {
+                if(movie.NumberAvailable == 0)
+                {
+                    return BadRequest("Movie is not available");
+                }
+                movie.NumberAvailable--;
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now,
+                };
+                _context.Rentals.Add(rental);
+            }
+            return Ok();
+        }
+    }
+}
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -27,6 +27,7 @@ namespace Vidly.Models
         [Display(Name = "Number in Stock")]
         [Range(1,20)]
         public int NumberInStock { get; set; }
+        public int NumberAvailable { get; set; }
         public Movie()
         {
             DateAdded = DateTime.Now;
